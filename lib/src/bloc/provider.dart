9d5cfb0f8671ab9eade210e4dfed51d50cddcb79
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-11-13 22:54:08 -0500

    Implementing Provider Widget extending from Inherited widget in order to update the widget three in entire app when it's neccesary, it's to say when updateShouldNotify it's true.

---
+++ lib/src/bloc/provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:forms_validation/src/bloc/login_bloc.dart';
+
+class Provider extends InheritedWidget{
+
+  final loginBloc = LoginBloc();
+
+  Provider({Key key, Widget child})
+    : super(key:key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    //Its almost always true 99% times
+    return true;
+  }
+
+  static LoginBloc of(BuildContext context){
+    return (context.inheritFromWidgetOfExactType(Provider) as Provider).loginBloc;
+  }
+
+}
