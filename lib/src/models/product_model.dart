928aad2655ce190650b430893a1b6e92b812d989
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-11-15 22:50:54 -0500

    Creating product model. Important tool: https://app.quicktype.io/?share=4Ik8Upww0mN33e2CBVmq

---
+++ lib/src/models/product_model.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+Product productFromJson(String str) => Product.fromJson(json.decode(str));
+
+String productToJson(Product data) => json.encode(data.toJson());
+
+class Product {
+    String id;
+    String title    = '';
+    double value    = 0.0;
+    bool available = true;
+    String fotoUrl;
+
+    Product({
+        this.id,
+        this.title,
+        this.value,
+        this.available,
+        this.fotoUrl,
+    });
+
+    factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id:         json["id"],
+        title:      json["title"],
+        value:      json["value"],
+        available:  json["available"],
+        fotoUrl:    json["fotoUrl"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id":         id,
+        "title":      title,
+        "value":      value,
+        "available": available,
+        "fotoUrl":    fotoUrl,
+    };
+}
